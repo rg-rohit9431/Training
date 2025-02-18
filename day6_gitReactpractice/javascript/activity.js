dc2030d5d18e3c34936ba814b4dc4de3d68e93f0
Rohit Gupta <9431rohitkumar@gmail.com>
2025-02-18 18:02:34 +0530

    add javascript work

---
+++ day6_gitReactpractice/javascript/activity.js
@@ -0,0 +1,22 @@
+class Cart{
+    constructor(){
+        this.cart=[];
+    }
+
+    addItem(product){
+        //by default quantity will be 1
+    }
+
+    removeItem(product){
+
+    }
+
+    updateItem(){
+        //increase the quantity by 1
+    }       
+
+    showTotalAmount(){
+
+    }
+}
+
